52a68eaf37c07d88b2148e46d98b7824f5ccfc3a
Aditya Kauthkar <adityakauthkar2021.it@mmcoe.edu.in>
2026-01-31 15:55:16 +0000

    dashboard

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,20 +1,31 @@
-import { PlusIcon, UploadCloudIcon } from "lucide-react";
+import { PlusIcon, UploadCloudIcon, XIcon } from "lucide-react";
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 const Dashboard = () => {
-  const colors = ['#9333ea' ]; 
-  // const [resumes, setResumes] = useState();
+  const colors = ["#9333ea"];
+  // const [allresumes, setAllResumes] = useState();
+  const [showCreateResume, setshowCreateResume] = useState(false);
+  const [showuploadResume, setshowuploadResume] = useState(false);
+  const [title, setTitle] = useState("");
+  const [resume, setResume] = useState(null);
+  const [editResumeId, seteditResumeId] = useState("");
+  const navigate = useNavigate();
+
   // const fetchAllResumes = async () => {
   //   const url = "";
   //   const response = await axios.get("");
   // };
 
+  const createResume = async (event) => {
+    event.preventDefault(); //prevent webpage from reloading
+    setshowCreateResume(false);
+    navigate("/resumebuilder");
+  };
+
   // useEffect(() => {
   //   fetchAllResumes();
   // }, []);
-
-  
 
   return (
     <div>
@@ -24,7 +35,10 @@ const Dashboard = () => {
         </p>
 
         <div className="flex gap-4">
-          <button className="w-full h-48 bg-white sm:max-w-36 flex flex-col items-center justify-center rounded-lg gap-2 text-slate-600 border border-dashed border-slate-600 group hover:border-indigo-500 hover:shadow-lg transition-all duration-300 cursor-pointer ">
+          <button
+            onClick={() => setshowCreateResume(true)}
+            className="w-full h-48 bg-white sm:max-w-36 flex flex-col items-center justify-center rounded-lg gap-2 text-slate-600 border border-dashed border-slate-600 group hover:border-indigo-500 hover:shadow-lg transition-all duration-300 cursor-pointer "
+          >
             <PlusIcon className="transition-all duration-300 size-11 p-2.5 bg-gradient-to-br from-indigo-300 to-indigo-500 text-white rounded-full" />
 
             <p className="text-sm transition-all duration-300 group-hover:text-indigo-600">
@@ -41,10 +55,46 @@ const Dashboard = () => {
           </button>
         </div>
 
-        <hr className="borderslate-300 my-6 sm:w-[305x]" />
+        <hr className="borderslate-300 my-6 sm:w-[305px]" />
         <div className="grid grid-cols-2 sm:flex flex-wrap gap-4">
           {/* map function to display the existing resumes  */}
+        </div>
 
+        {/* Modal after opening create resume */}
+
+        <div>
+          {showCreateResume && (
+            <form
+              onSubmit={createResume}
+              onClick={() => setshowCreateResume(false)}
+              className="fixed inset-0 bg-black/70 backdrop-blur bg-opacity-50 z-10 flex items-center justify-center "
+            >
+              <div
+                className="relative bg-slate-50 border shadow-md rounded-lg w-full max-w-sm p-6"
+                onClick={(e) => e.stopPropagation()}
+              >
+                <h1> Create a resume </h1>
+                <input
+                  type="text"
+                  placeholder="Enter a resume title "
+                  className="w-full px-4 py-2 mb-4 focus:border-green-600"
+                  // value={title}
+                  required
+                />
+                <button className="w-full py-2 bg-green-600 text-white hover:bg-green-700 rounded hover:bg-green-700 transition-colors ">
+                  Create Resume
+                </button>
+
+                <XIcon
+                  className=" absolute top-4 right-4 text-slate-500 hover:text-slate-50 cursor-pointer transition-colors  "
+                  onClick={() => {
+                    setshowCreateResume(false);
+                    setTitle("");
+                  }}
+                />
+              </div>
+            </form>
+          )}
         </div>
       </div>
     </div>
